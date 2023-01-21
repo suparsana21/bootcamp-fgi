fe43859b88fb50befe7e765b3395dbd990c3277d
suparsana <madesuparsana29@gmail.com>
2023-01-21 12:28:03 +0800

    Add token generate

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,10 @@
 from django.urls import path
 
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('', views.UserList.as_view()),    
+    path('token', TokenObtainPairView.as_view()),
+    path('refresh', TokenRefreshView.as_view())
 ]
